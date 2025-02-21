b2cb3aefa7c96d2f3165b6a4c42d37a32a7a3bdd
vivek.kumar <vivek.kumar@appinventiv.com>
2025-02-21 12:37:49 +0530

    routing module changes

--- pro-coder-inventory/src/app/app.module.ts
+++ pro-coder-inventory/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { BsModalService, ModalModule } from 'ngx-bootstrap/modal';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -11,7 +12,8 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { SettingsModalComponent } from './components/settings-modal/settings-modal.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
+
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,11 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   bootstrap: [AppComponent],
   providers: [provideAnimationsAsync(), BsModalService],
-  imports: [AppRoutingModule, BrowserModule, BsDropdownModule, ModalModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    BsDropdownModule,
+    ModalModule,
+  ],
 })
 export class AppModule {}
--- pro-coder-inventory/src/app/app.routes.ts
+++ pro-coder-inventory/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+];
